a25da13784b00a148f1d5c6b1cfeecf549865957
Damien <damienmail91@gmail.com>
2017-05-30 16:35:09 +0200

    Template-book : modifie la page pour ne pas utiliser le plugin Advanced Custom Fields et mise en forme robuste a l'absence de certains custom fields

--- template-book.php
+++ template-book.php
@@ -14,29 +14,68 @@ get_header();
     
     <div class="container">
         <div class="row">
-            <div class="col-md-3 img-responsive center-block"><?php 
+            <div class="col-md-3 img-responsive center-block">
+            <?php 
 
 			if ( has_post_thumbnail() ) {
 			the_post_thumbnail( array(800,3200) );
-			}  ?>
+			}  
+
+			$book_page_id = get_the_id();
+
+			$book_title = get_post_meta( $book_page_id, 'title', true);
+			$book_author = get_post_meta( $book_page_id, 'author', true);
+			$book_summary = get_post_meta( $book_page_id, 'summary', true);
+			$book_recommendation = get_post_meta( $book_page_id, 'rec', true);
+
+			$book_amazon_url = get_post_meta( $book_page_id, 'amazon', true);
+			$book_amazon_logo = get_post_meta( $book_page_id, 'buy1', true);
+
+			$book_leslibraires_url = get_post_meta( $book_page_id, 'leslibraires', true);
+			$book_leslibraires_logo = get_post_meta( $book_page_id, 'buy2', true);
+
+			$book_fnac_url = get_post_meta( $book_page_id, 'fnac', true);
+			$book_fnac_logo = get_post_meta( $book_page_id, 'buy3', true);
 			
+			?>
+
 			</div>
             	<div class="col-md-8">
 
-            		<h1 class="booktitle">Titre du livre : <?php the_field('title'); ?> </br>
-            	    Auteur : <?php the_field('author'); ?></h1>
+            		<h1 class="booktitle">Titre : <?php echo $book_title; ?> </br>
+            	    Auteur : <?php echo $book_author; ?></h1>
             		
-            		<p class="summary"><b>Résumé du livre : </b><?php the_field('summary'); ?></p>
-            		
-            		<p class="rec"><b>Recommandation de Steve : </b><?php the_field('rec'); ?></p>
-            		
-            		<h4> ACHETER LE LIVRE SUR : </h4>
+            		<?php
+            		if ( $book_summary ) : ?>
+	            		<p class="summary"><b>Résumé : </b><?php echo $book_summary; ?></p>
+	            	<?php 
+	            	endif;
+	            	
+            		if ( $book_recommendation ) : ?>	
+            			<p class="rec"><b>Recommandation de Steve : </b><?php echo $book_recommendation; ?></p>
+            		<?php 
+	            	endif; ?>
+
+            		<h4> Acheter ce livre : </h4>
             	    
-            	    <a href="<?php the_field('amazon'); ?>"> <img src="<?php the_field('buy1'); ?>" alt="" /> </a>
-            	    
-            	    <a href="<?php the_field('leslibraires'); ?>"> <img src="<?php the_field('buy2'); ?>" alt="" /> </a>
-            	    
-            	    <a href="<?php the_field('fnac'); ?>"> <img src="<?php the_field('buy3'); ?>" alt="" /> </a>
+            	    <?php 
+            	    if ( $book_amazon_logo && $book_amazon_url ) : ?>	
+            	    	<a href="<?php echo $book_amazon_url; ?>" title="Amazon"> <img src="<?php echo $book_amazon_logo; ?>" alt="" /> </a>
+            	    <?php 
+            	    endif; ?>
+
+            	    <?php 
+            	    if ( $book_leslibraires_logo && $book_leslibraires_url ) : ?>
+	            	    <a href="<?php echo $book_leslibraires_url; ?>" title="Les Libraires"> <img src="<?php echo $book_leslibraires_logo; ?>" alt="" /> </a>
+            	    <?php 
+            	    endif; ?>
+
+            	    <?php 
+            	    if ( $book_fnac_logo && $book_fnac_url ) : ?>
+	            	    <a href="<?php echo $book_fnac_url; ?>" title="La Fnac"> <img src="<?php echo $book_fnac_logo; ?>" alt="" /> </a>
+	            	<?php 
+            	    endif; ?>
+
 				</div>
 
             <!-- end row -->
