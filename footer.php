6f10a7f977a185a4fa55c199a455ccadc83855cf
guilhem <guilhem.menard@gmail.com>
2017-06-01 15:45:50 +0200

    modificationcréation du footer en trois parties bootstrap

--- footer.php
+++ footer.php
@@ -1,12 +1,40 @@
 <footer class="full-width-container primary-footer">
     <div class="container">
         <div class="row" >
+            <div class="column1 col-md-4" > <!-- beginning column1-->
+                    <a href="<?php echo home_url( '/' ); ?>">
+                    <img class="logo-uploaded" style="max-width:<?php echo esc_attr( get_theme_mod( 'themeora-img-upload-logo-width', '200' ) ); ?>px" src="<?php echo esc_url( get_theme_mod( 'themeora-img-upload-logo' ) );?>" alt="<?php the_title(); ?>" />
+                    </a>            
+                    <p class="about"><label> Yeswekant.com est une librairie en ligne. Découvrez sur notre site les livres qui ont marqué les personnalités qui vous inspirent. </label></p>
+            </div>
             <?php if ( is_active_sidebar( 'footer-widget' ) ) : ?>
-                <div id="widget-area" class="widget-area col-md-2 col-md-offset-10">
-                    <?php dynamic_sidebar( 'footer-widget' ); ?>
-                </div><!-- .widget-area -->
+            <div id="widget-area" class="widget-area col-md-4">
+            <i class="fa fa-envelope fa-3x"></i> 
+            </br>
+            <p>
+            <label> Nous contacter </label>
+            </br>
+            Des remarques, des commentaires ?
+            Envoyez nous un mail à contact@yeswekant.com
+            <?php dynamic_sidebar( 'footer-widget' ); ?>
+            </div><!-- .widget-area -->
             <?php endif; ?>
+            <div class="column3 col-md-4" > <!-- beginning column1-->
+                    <i class="fa fa-telegram fa-3x"></i> 
+                    </br>
+                    <p>
+                    <label>Toutes les semaines, une nouvelle personnalité dans votre boite mail</label>
+                    <input class="news" type="email" name="EMAIL" placeholder="Entrez votre mail" required />
+                    </br>
+                    <input type="submit" value="> Je m'inscris"/>
+                    </p>
+            </div>
+        
         </div><!-- end row -->
+
+        <div >
+<p>© 2017 - YesWeKant | Home | Mentions légales | Conditions d’utilisation | Utilisation des cookies | Plan du site </p>
+            </div>
         
         <?php if ( has_nav_menu( 'social_menu' ) ) : ?>
             <div class="row">
