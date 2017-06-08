123f8c85d9791b672dfdfe1462dbe110a9585214
guilhem <guilhem.menard@gmail.com>
2017-06-08 10:28:49 +0200

    deleted search template to include it in the search.php file

--- search.php
+++ search.php
@@ -70,11 +70,41 @@ $layout = get_theme_mod('themeora_blog_layout', 'full-width');
 
 
 <?php if ( have_posts() ) : ?>
-
     
 
-    <?php get_template_part( 'template-search' ); ?>
+<div class="full-width-container main-content-area">
 
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-6 col-md-offset-3">
+
+                <?php if ( have_posts() ) : ?>
+
+                    <div id="posts-wrapper">
+
+                        <?php while ( have_posts() ) : the_post(); ?>
+
+                            <?php get_template_part( 'content', get_post_format() ); ?>
+
+                        <?php endwhile; ?>
+
+                    </div>
+
+                <?php endif; ?>
+
+                <?php themeora_paging(); ?>
+
+            </div><!-- col-md-8 -->
+
+            <?php get_sidebar(); ?>
+
+        </div><!-- row -->
+
+    </div><!-- container -->
+
+</div>
    
 
 <?php else : ?>
