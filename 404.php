e5b7622b2177a85bba77af3731da9491da81feb6
guilhem <guilhem.menard@gmail.com>
2017-06-12 00:30:02 +0200

    responsiveness of the footer is improved + added a page 404.php in French

---
+++ 404.php
@@ -0,0 +1,41 @@
+<?php get_header(); 
+
+    // Get theme options
+
+?>
+
+
+
+<div class="full-width-container">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-10 col-md-offset-1 text-center">
+
+                <?php if ( ! have_posts() ) : ?>
+
+                    <h1 class="title"><?php _e('Désolé! La page que vous cherchez n\'existe pas.', 'oren'); ?></h1>
+                    
+                    <p>
+                        
+                        <?php _e( 'Rechercher une autre page :', 'oren' ); ?>
+
+                        <?php get_search_form(); ?>
+
+                    </p>
+
+                <?php endif; ?>
+
+            </div><!-- col-md-10 -->
+
+        </div><!-- end row -->
+
+    </div><!-- end container -->
+
+</div><!-- end full-width-container -->
+
+
+
+<?php get_footer(); ?>
