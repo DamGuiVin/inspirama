1b39a4e976e7fb3076b171981d127b9bf3b98a41
Damien <damienmail91@gmail.com>
2017-07-03 14:52:41 +0200

    Header: ajout du scipt de tracking de Google Analytics

--- header.php
+++ header.php
@@ -29,6 +29,24 @@
 
 
 
+    <!-- Google Analytics Tracking Code for www.yeswekant.com -->
+
+    <script>
+
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-98638124-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
+    <!-- End Google Analytics Tracking Code for www.yeswekant.com -->
+
+
+
     <!-- JS code for automatic smooth scrolling to anchor links -->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
