2db1c59223915c733d79c0a53452947cd19d9811
Damien <damienmail91@gmail.com>
2017-08-05 16:41:14 +0200

    Person + Book : committing all commit of preview branch into master

--- single-person.php
+++ single-person.php
@@ -116,38 +116,88 @@ if ( $wp_query ) :
 
         <div class="portfolio-books-wrapper">
             <ul id="masonry-wrapper" class="portfolio-cols-4">
-                <?php 
+            
+            <?php 
 
-                // The iterator will let us keep track of at which Book in the Recommendation array we are
-                $iterator = 0;
+            // The iterator will let us keep track of at which Book in the Recommendation array we are
+            $iterator = 0;
 
-                while ( $wp_query->have_posts() ) : 
+            // We load the sellers logos only once : outside of the while loop
+            $book_leslibraires_logo = get_stylesheet_directory_uri() . '/img/les_libraires.png' ;
+            $book_amazon_logo = get_stylesheet_directory_uri() . '/img/amazon.png' ;
+            $book_fnac_logo = get_stylesheet_directory_uri() . '/img/fnac.png' ;
+            $book_priceminister_logo = get_stylesheet_directory_uri() . '/img/priceminister.png' ;
+            $book_recyclivre_logo = get_stylesheet_directory_uri() . '/img/recyclivre.png' ;
+            $book_ebook_logo = get_stylesheet_directory_uri() . '/img/ebooks.png' ;
+            $book_gutenberg_logo = get_stylesheet_directory_uri() . '/img/gutenberg.png' ;
 
-                    $wp_query->the_post();
+            while ( $wp_query->have_posts() ) :
 
-                    // Recover useful attributes from the Book
-                    $book_page_id = $wp_query->post->ID;
-                    $book_title = get_the_title();
-                    $book_author = get_post_meta( $book_page_id, 'author', true);
+                $wp_query->the_post();
 
-                    /* 
-                    PAS NECESSAIRE POUR L'INSTANT MAIS SERA UTILE POUR L'AFFICHAGE DE LA RECOMMENDATION DEPUIS LA PAGE PERSON
+                // Recover useful attributes from the Book
+                $book_page_id = $wp_query->post->ID;
+                $book_title = get_the_title();
+                $book_author = get_post_meta( $book_page_id, 'author', true);
+                $book_genre = get_post_meta( $book_page_id, 'genre', true);
+                $book_theme = get_post_meta( $book_page_id, 'theme', true);
+                $book_rewards = get_post_meta( $book_page_id, 'rewards', true);
 
-                    // Recover useful attributes from the Recommendation
-                    $recommendation = $recommendations_array[ $iterator ];
-                    $recommendation_id = $recommendation->term_id;
-                    $recommendation_text = $recommendation->description;
-                    $recommendation_sources_titles = get_term_meta( $recommendation_id, 'sources_titles', true);
-                    $recommendation_sources_urls = get_term_meta( $recommendation_id, 'sources_urls', true);
-                    */
+                // Sellers URLs
+                $book_leslibraires_url = get_post_meta( $book_page_id, 'leslibraires_url', true );
+                $book_amazon_url = get_post_meta( $book_page_id, 'amazon_url', true );
+                $book_fnac_url = get_post_meta( $book_page_id, 'fnac_url', true );
+                $book_priceminister_url = get_post_meta( $book_page_id, 'priceminister_url', true );
+                $book_recyclivre_url = get_post_meta( $book_page_id, 'recyclivre_url', true );
+                $book_ebook_url = get_post_meta( $book_page_id, 'ebook_url', true );
+                $book_gutenberg_url = get_post_meta( $book_page_id, 'gutenberg_url', true );
 
-                    // Check if the Book has an image. Only load the Book if it does
-                    if ( has_post_thumbnail( $book_page_id ) ) {
-                        $previewImage = wp_get_attachment_image_src( get_post_thumbnail_id( $book_page_id ), $img_size ); ?>
+                // Recover useful attributes from the Recommendation
+                $recommendation = $recommendations_array[ $iterator ];
+                $recommendation_id = $recommendation->term_id;
+                $recommendation_text = $recommendation->description;
+                $recommendation_sources_titles = get_term_meta( $recommendation_id, 'sources_titles', true);
+                $recommendation_sources_urls = get_term_meta( $recommendation_id, 'sources_urls', true);
 
-                        <!--  Book Frame -->
-                        <li class="masonry-item">
-                            <a href="<?php the_permalink(); ?>" class="portfolio-link">
+
+                // Check if the Book has an image. Only load the Book if it does
+                if ( has_post_thumbnail( $book_page_id ) ) {
+                    $previewImage = wp_get_attachment_image_src( get_post_thumbnail_id( $book_page_id ), $img_size ); ?>
+
+                    <!--  Book Frame -->
+                    <li class="masonry-item">
+                        <figure 
+                            data-person="<?php echo $person_name; ?>"
+
+                            data-title="<?php echo $book_title; ?>" 
+                            data-author="<?php echo $book_author; ?>"
+
+                            data-link="<?php echo the_permalink(); ?>"
+                            data-genre="<?php echo $book_genre; ?>"
+                            data-theme="<?php echo $book_theme; ?>"
+                            data-rewards="<?php echo $book_rewards; ?>"
+
+                            data-recommendation="<?php echo htmlspecialchars( $recommendation_text ); ?>"
+                            data-sources-titles="<?php echo $recommendation_sources_titles; ?>"
+                            data-sources-urls="<?php echo $recommendation_sources_urls; ?>"
+
+                            data-leslibraires-url="<?php echo $book_leslibraires_url; ?>"
+                            data-amazon-url="<?php echo $book_amazon_url; ?>"
+                            data-fnac-url="<?php echo $book_fnac_url; ?>"
+                            data-priceminister-url="<?php echo $book_priceminister_url; ?>"
+                            data-recyclivre-url="<?php echo $book_recyclivre_url; ?>"
+                            data-ebook-url="<?php echo $book_ebook_url; ?>"
+                            data-gutenberg-url="<?php echo $book_gutenberg_url; ?>"
+
+                            data-leslibraires-logo="<?php echo $book_leslibraires_logo; ?>"
+                            data-amazon-logo="<?php echo $book_amazon_logo; ?>"
+                            data-fnac-logo="<?php echo $book_fnac_logo; ?>"
+                            data-priceminister-logo="<?php echo $book_priceminister_logo; ?>"
+                            data-recyclivre-logo="<?php echo $book_recyclivre_logo; ?>"
+                            data-ebook-logo="<?php echo $book_ebook_logo; ?>"
+                            data-gutenberg-logo="<?php echo $book_gutenberg_logo; ?>"
+                            >
+                            <a href="#" class="portfolio-link">
                                 
                                 <!--  Book Image Button Effect -->
                                 <div class="button-effect">
@@ -179,17 +229,16 @@ if ( $wp_query ) :
                                 </div>
 
                             </a>
-                        </li>
-                        <!-- End Book Frame -->
+                        </figure>
+                    </li>
+                    <!-- End Book Frame -->
 
-                    <?php } 
+                <?php } 
+                ++$iterator; 
 
-                    ++$iterator; 
-
-                endwhile; ?>
+            endwhile; ?>
             </ul>
-        </div>
-        <!-- Books Mosaic -->  
+        <!-- End Books Mosaic -->  
 
     <?php
     wp_reset_query();
@@ -208,6 +257,18 @@ else : ?>
     </div>
 
 <?php endif;
+
+
+
+
+
+
+
+
+
+
+
+
 
 //........................................................................
 // FOOTER SECTION 
