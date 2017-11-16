987bdd454d41aea51d2acc77644e3b33e5483719
Damien <damienmail91@gmail.com>
2017-11-16 12:31:49 +0100

    Carousels : BIG COMMIT : setting up a global framework for inspirama carousels, with or without dropdowns, with different actions based on the type of items inside. Impacts many files, will now allow the creation of many types of carousels with filters (in particular, people carousel for the HP)

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,6 @@
 (function($) {
 
-    $("#books-carousel-people-categories a").on('click', function (event) {
+    $(".inspirama-carousel-dropdown a").on('click', function (event) {
 
         // Prevent the # link to send us back to the top of the page
         event.preventDefault();
@@ -17,43 +17,57 @@
             category_button.append( '<span class="caret"></span>' );
 
             // Save the carousel jquery object
-            var books_carousel = $(this).parents('h2').siblings('.carousel.slide');
+            var books_carousel = $(this).parents('h2').siblings('.inspirama-carousel');
 
             // The loading gif comes over and hides the carousel item
-            $('#loading-image').fadeIn(150);
+            var loading_image = books_carousel.find('.ajax-loading');
+            loading_image.fadeIn(150);
+
+            // Build the AJAX according to the type of carousel
+            var carousel_item_classes = books_carousel.find(".item").children().attr('class');
+            var ajax_data = {};
+            var ajax_success_callback = new Function();
+
+            if ( carousel_item_classes.indexOf('books-carousel-item') != -1 ){
+                ajax_data = {
+                    'category_name' : category_name,
+                    'action': 'inspirama_get_books_carousel'
+                };
+                ajax_success_callback = $.update_books_carousel;
+            }
 
             // Ajax call
             $.ajax({
                 type: 'POST',
                 url: ajax_object.ajax_url,
-                data: {
-                    'category_name' : category_name,
-                    'action': 'inspirama_get_books_carousel'
-                }, 
-
-                success: function (response) {
-
-                    // Update the content of each of the already-existing carousel items
-                    // NB: THIS ASSUMES THERE IS ALWAYS THE SAME NUMBER IN EACH CATEGORY
-                    books_carousel.find(".item").each(
-                        function ( i ) { 
-                            $(this).find(".recommended-book").each(
-                                function ( j ) {
-                                    
-                                    $(this).find("h3").text( response.selected_books[i][j]['book_title'] );
-                                    $(this).find("h4").text( response.selected_books[i][j]['book_author'] );
-                                    $(this).find("a").attr("href", response.selected_books[i][j]['book_url'] );
-                                    $(this).find("img").attr("src", response.selected_books[i][j]['book_image'] );
-                                }); 
-                        });
-
-                    // The loading gif progressively disappears
-                    $('#loading-image').fadeOut(1000);
+                data: ajax_data, 
+                success: function (response) { 
+                    ajax_success_callback( response, books_carousel );
+                    loading_image.fadeOut(1000); 
                 },
-
-                error: function () {}
+                error: function () {
+                    loading_image.fadeOut(1000); 
+                }
             });
+    
         }
     });
+
+    $.update_books_carousel = function( response, books_carousel ) {
+
+        // Update the content of each of the already-existing books carousel items
+        // NB: THIS ASSUMES THERE IS ALWAYS THE SAME NUMBER IN EACH CATEGORY
+        books_carousel.find(".item").each(
+            function ( i ) { 
+                $(this).find(".recommended-book").each(
+                    function ( j ) {
+                        
+                        $(this).find("h3").text( response.selected_books[i][j]['book_title'] );
+                        $(this).find("h4").text( response.selected_books[i][j]['book_author'] );
+                        $(this).find("a").attr("href", response.selected_books[i][j]['book_url'] );
+                        $(this).find("img").attr("src", response.selected_books[i][j]['book_image'] );
+                    }); 
+            });
+    };
 
 })(jQuery)
--- template-home.php
+++ template-home.php
@@ -41,41 +41,31 @@ $background_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->I
 </header>
 
 
-<!-- Top Recommendations Carousel -->
-<?php recommendations_carousel( array(  
-    'les-freres-karamazov', 
-    'le-cycle-de-fondation-tome-2-fondation-et-empire',
-    'vagabonding',
-    'le-petit-prince') ); ?>
+
+<?php 
+//........................................................................
+// CAROUSELS SECTION
+//........................................................................
+
+    $args['books_slugs_array'] = array(  
+        'les-freres-karamazov', 
+        'le-cycle-de-fondation-tome-2-fondation-et-empire',
+        'vagabonding',
+        'le-petit-prince');
+
+    inspirama_carousel_wrapper( 
+        $carousel_title = 'Carroussel de recommandations',
+        array(),
+        $carousel_type = 'recommendations',
+        $args);
+    
+    inspirama_carousel_wrapper( 
+        $carousel_title = 'Carroussel de livres',
+        array('Artistes', 'Entrepreneurs', 'Politiciens', 'Autres'),
+        $carousel_type = 'books');
 
 
-<!-- Top Books Section -->
-<div class="container-fluid">
-    <div class="row">
-        <div class="WAZAAAAA">
-            
-            <h2>Les livres recommandés par les 
-                <div id="books-carousel-people-categories" class="dropdown books-carousel-people-categories">
-                    <button class="btn btn-basic dropdown-toggle" type="button" data-toggle="dropdown">
-                        ______<span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#books-carousel-people-categories">Entrepreneurs</a></li>
-                        <li><a href="#books-carousel-people-categories">Artistes</a></li>
-                        <li><a href="#books-carousel-people-categories">Politiciens</a></li>
-                        <li><a href="#books-carousel-people-categories">Autres</a></li>
-                    </ul>
-                </div>
-            </h2>
 
-            <?php books_carousel(); ?>
-
-        </div>
-    </div>
-</div>
-
-
-<?php
 //........................................................................
 // FOOTER SECTION 
 //........................................................................
